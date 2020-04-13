4a7b8b54d50432316a4e28ab6507d00137eb6db7
Danila Vershinin <250071+dvershinin@users.noreply.github.com>
2020-04-13 22:15:57 +0300

    up

--- prepare.sh
+++ prepare.sh
@@ -9,6 +9,8 @@ if [[ "${RHEL}" -ge "8" ]]; then
   dnf -y install yum-utils
   dnf -y module reset php
   dnf -y module install php:remi-7.2
+  # composer is in there:
+  dnf config-manager --enable remi
 else
   REPO=remi-php56
   yum -y install yum-utils
