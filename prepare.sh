48de3da8320c5bfb1a55cadda0fa2d01fe580082
Danila Vershinin <ciapnz@gmail.com>
2026-04-21 22:46:29 +0800

    Fix: bump PHP to 8.2 for n98-magerun2 9.x (requires PHP >= 8.1)
    prepare.sh installed php:remi-7.4 module, but n98-magerun2 9.x composer
    dependencies require PHP >= 8.1.0. Every build (all distros/arches) failed
    in %build when Box compiled the phar:
    
      "Your Composer dependencies require a PHP version >= 8.1.0.
       You are running 7.4.33."
    
    Bump to php:remi-8.2 (EL8+/Fedora) and remi-php82 (EL7/amzn2). Also bump
    runtime Requires: php(language) >= 8.1.
    
    Verified with rebuild.sh --fast --dist el8: phar compiled and verified,
    all three RPMs built.
    
    Failing job was build-deploy-amzn2-aarch64 but issue is global, not
    arch-specific.

--- prepare.sh
+++ prepare.sh
@@ -5,27 +5,21 @@ RHEL=$(rpm -E 0%{?rhel} | bc)
 FEDORA=$(rpm -E 0%{?fedora} | bc)
 yum -y install http://rpms.remirepo.net/enterprise/remi-release-${RHEL}.rpm
 
-# n98-magerun2 supports only PHP 7.2 and above
+# n98-magerun2 9.x requires PHP >= 8.1
 if [[ "${RHEL}" -ge "8" ]] || [[ "${FEDORA}" -ge "33" ]]; then
   dnf -y install yum-utils
   dnf -y module reset php
-  dnf -y module install php:remi-7.4
+  dnf -y module install php:remi-8.2
   # new composer is in there:
   dnf config-manager --enable remi
   dnf -y install composer
   dnf -y update composer
 else
-  REPO=remi-php74
+  REPO=remi-php82
   yum -y install yum-utils
   yum-config-manager --enable remi
   yum-config-manager --enable ${REPO}
-fi
-
-# php-pear-phing in remi is no good
-# no dependencies in EL8 for php-pear-phing, using phing's phar
-if [[ "${RHEL}" -le "7" ]]; then
-  yum -y install --disablerepo=remi* php-pear-phing
-  # ensure upgrading system PHP to 7.4
+  # ensure upgrading system PHP to 8.2
   yum -y upgrade
 fi
 
